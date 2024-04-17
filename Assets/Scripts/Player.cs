9f60ba071a7eb9ae6b2d5797cec0543ccfec78d5
camipaula <103073699+camipaula@users.noreply.github.com>
2024-04-16 20:36:24 -0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float jumpForce = 3.3f;
+    private Rigidbody Physics;
+
+    [SerializeField]
+    int playerSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Physics.AddForce(new Vector3(0,jumpForce,0), ForceMode.Impulse);
+        }
+        
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "obstaculos")
+        {
+            
+            Destroy(gameObject);
+            Debug.Log("Perdiste");
+            
+        }
+    }
+}
